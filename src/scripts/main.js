8a4b9674a17559e970d310fd826ff12b2ff69807
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2024-10-15 14:09:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,20 @@
 const fieldText = document.querySelectorAll('.field-text');
 
 function splitCamelCase(str) {
-  return str.replace(/([a-z])([A-Z])/g, '$1 $2');
+  let string = str;
+
+  if (string.charAt(0).toUpperCase() === string.charAt(0)) {
+    string = ' ' + string;
+  }
+
+  return string.replace(/([a-z])([A-Z])/g, '$1 $2').trim();
 }
 
 function capitalizeWords(str) {
+  if (str === null || str === undefined) {
+    return '';
+  }
+
   return str
     .split(' ')
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
@@ -15,15 +25,16 @@ function capitalizeWords(str) {
 }
 
 fieldText.forEach((input) => {
+  const InputName = 'name';
   const label = document.createElement('label');
-  const nameInput = input.getAttribute('name')
-    ? splitCamelCase(input.getAttribute('name'))
+  const nameInput = input.getAttribute(InputName)
+    ? splitCamelCase(input.getAttribute(InputName))
     : null;
   const capitalizedWords = capitalizeWords(nameInput);
   const parentInput = input.parentNode;
 
   label.classList.add('field-label');
-  label.setAttribute('for', input.getAttribute('name'));
+  label.setAttribute('for', input.getAttribute(InputName));
   label.textContent = nameInput.toUpperCase();
 
   input.setAttribute('placeholder', capitalizedWords);
